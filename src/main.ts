b2c7daaf5e226ba9e07cceb7aa0f5dd04a237fd6
maproute39-hue <maproute39@gmail.com>
2026-02-12 20:53:25 -0400

    Set Mapbox access token

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 import Mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
  
-Mapboxgl.accessToken = '';
+Mapboxgl.accessToken = 'pk.eyJ1IjoiZnJ1dG1lIiwiYSI6ImNsMDQ2MW02OTBicWMzYnF3bnZ6aWs0dmcifQ.J6L3_p4NQ9G7LdyIsPWbow';
 
 if (!navigator.geolocation){
   alert("navegador no soportado");
